004d1adad0991aa74d6f282c7af50bbf10d9005d
demeu <demeukuanysbek@gmail.com>
2024-01-25 16:06:49 +0600

    All app colors are changed

--- lib/constants/app_color.dart
+++ lib/constants/app_color.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color mainColor = Color(0xFFE03333);
-  static const Color secondColor = Color(0xFFFF9E9E);
-  static const Color thirdColor = Color(0xFFFFD5D5);
-  static const Color fourthColor = Color(0xFFEBE6E6);
-  static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color blackColor = Color(0xFF000000);
+  static const Color mainColor = Color(0xFF000000);
+  static const Color secondColor = Color(0xFFFFFFFF);
+  static const Color thirdColor = Color(0xFFEBE6E6);
+  static const Color fourthColor = Color(0xFFFFD5D5);
+  static const Color whiteColor = Color(0xFFFF9E9E);
+  static const Color blackColor = Color(0xFFE03333);
 }
