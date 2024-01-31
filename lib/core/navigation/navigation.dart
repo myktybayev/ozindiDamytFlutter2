e39fe6c5283c1410043f8364affe5481d0b5091f
demeu <demeukuanysbek@gmail.com>
2024-01-31 15:13:23 +0600

    drawer

--- lib/core/navigation/navigation.dart
+++ lib/core/navigation/navigation.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:ozindi_damyt/core/navigation/my_header_drawer.dart';
-import 'package:ozindi_damyt/features/cinema/cinema_page.dart';
-import 'package:ozindi_damyt/features/hobby/hobby_page.dart';
 import 'package:ozindi_damyt/features/library/library_page.dart';
-import 'package:ozindi_damyt/features/maraphone/maraphone_page.dart';
-import 'package:ozindi_damyt/features/new_recommendation/new_recommendation_page.dart';
-import 'package:ozindi_damyt/features/podcast/pages/podcast_page.dart';
-import 'package:ozindi_damyt/features/proforintation/proforintation_page.dart';
-import 'package:ozindi_damyt/features/quiz/quiz_page.dart';
-import 'package:ozindi_damyt/features/sport/sport_page.dart';
-import 'package:ozindi_damyt/features/stoury/stoury_page.dart';
+
+import '../../features/cinema/cinema_page.dart';
+import '../../features/hobby/hobby_page.dart';
+import '../../features/maraphone/maraphone_page.dart';
+import '../../features/new_recommendation/new_recommendation_page.dart';
+import '../../features/podcast/pages/podcast_page.dart';
+import '../../features/proforintation/proforintation_page.dart';
+import '../../features/quiz/quiz_page.dart';
+import '../../features/sport/sport_page.dart';
+import '../../features/stoury/stoury_page.dart';
 
 class DrawerMenu extends StatefulWidget {
   const DrawerMenu({super.key});
@@ -19,7 +20,7 @@ class DrawerMenu extends StatefulWidget {
 }
 
 class DrawerMenuState extends State<DrawerMenu> {
-  var currentPage;
+  var currentPage = DrawerSections.library;
 
   @override
   Widget build(BuildContext context) {
@@ -42,86 +43,79 @@ class DrawerMenuState extends State<DrawerMenu> {
     return Column(
       children: [
         menuItem(
-          1,
-          "Кітапхана",
-          "library.png",
-          currentPage == DrawerSections.library ? true : false,
-          MaterialPageRoute(builder: (BuildContext context) => LibraryPage()),
-        ),
+            1,
+            "Кітапхана",
+            "library.png",
+            currentPage == DrawerSections.library ? true : false,
+            MaterialPageRoute(
+                builder: (BuildContext context) => LibraryPage())),
         sb2(),
         menuItem(
-          2,
-          "Ғибратты әңгімелер",
-          "stoury.png",
-          currentPage == DrawerSections.stoury ? true : false,
-          MaterialPageRoute(builder: (BuildContext context) => StouryPage()),
-        ),
+            2,
+            "Ғибратты әңгімелер",
+            "stoury.png",
+            currentPage == DrawerSections.stoury ? true : false,
+            MaterialPageRoute(builder: (BuildContext context) => StouryPage())),
         sb2(),
         menuItem(
-          3,
-          "Подкаст",
-          "podcast.png",
-          currentPage == DrawerSections.podcast ? true : false,
-          MaterialPageRoute(builder: (BuildContext context) => PodcastPage()),
-        ),
+            3,
+            "Подкаст",
+            "podcast.png",
+            currentPage == DrawerSections.podcast ? true : false,
+            MaterialPageRoute(
+                builder: (BuildContext context) => PodcastPage())),
         sb2(),
         menuItem(
-          4,
-          "Quiziz",
-          "quiz.png",
-          currentPage == DrawerSections.quiz ? true : false,
-          MaterialPageRoute(builder: (BuildContext context) => QuizPage()),
-        ),
+            4,
+            "Quiziz",
+            "quiz.png",
+            currentPage == DrawerSections.quiz ? true : false,
+            MaterialPageRoute(builder: (BuildContext context) => QuizPage())),
         sb2(),
         menuItem(
-          5,
-          "Марафон",
-          "marathon.png",
-          currentPage == DrawerSections.marathon ? true : false,
-          MaterialPageRoute(builder: (BuildContext context) => MaraphonePage()),
-        ),
+            5,
+            "Марафон",
+            "marathon.png",
+            currentPage == DrawerSections.marathon ? true : false,
+            MaterialPageRoute(
+                builder: (BuildContext context) => MaraphonePage())),
         sb2(),
         menuItem(
-          6,
-          "Профориентация",
-          "proforintation.png",
-          currentPage == DrawerSections.proforintation ? true : false,
-          MaterialPageRoute(
-              builder: (BuildContext context) => ProforintationPage()),
-        ),
+            6,
+            "Профориентация",
+            "proforintation.png",
+            currentPage == DrawerSections.proforintation ? true : false,
+            MaterialPageRoute(
+                builder: (BuildContext context) => ProforintationPage())),
         sb2(),
         menuItem(
-          7,
-          "Хобби",
-          "hobby.png",
-          currentPage == DrawerSections.hobby ? true : false,
-          MaterialPageRoute(builder: (BuildContext context) => HobbyPage()),
-        ),
+            7,
+            "Хобби",
+            "hobby.png",
+            currentPage == DrawerSections.hobby ? true : false,
+            MaterialPageRoute(builder: (BuildContext context) => HobbyPage())),
         sb2(),
         menuItem(
-          8,
-          "Спорт",
-          "sport.png",
-          currentPage == DrawerSections.sport ? true : false,
-          MaterialPageRoute(builder: (BuildContext context) => SportPage()),
-        ),
+            8,
+            "Спорт",
+            "sport.png",
+            currentPage == DrawerSections.sport ? true : false,
+            MaterialPageRoute(builder: (BuildContext context) => SportPage())),
         sb2(),
         menuItem(
-          9,
-          "Кино",
-          "movies.png",
-          currentPage == DrawerSections.cinema ? true : false,
-          MaterialPageRoute(builder: (BuildContext context) => CinemaPage()),
-        ),
+            9,
+            "Кино",
+            "movies.png",
+            currentPage == DrawerSections.cinema ? true : false,
+            MaterialPageRoute(builder: (BuildContext context) => CinemaPage())),
         sb2(),
         menuItem(
-          10,
-          "Жаңа ұсыныстар",
-          "new_recommendation.png",
-          currentPage == DrawerSections.new_recommendation ? true : false,
-          MaterialPageRoute(
-              builder: (BuildContext context) => NewRecommendationPage()),
-        ),
+            10,
+            "Жаңа ұсыныстар",
+            "new_recommendation.png",
+            currentPage == DrawerSections.new_recommendation ? true : false,
+            MaterialPageRoute(
+                builder: (BuildContext context) => NewRecommendationPage())),
         sb2(),
       ],
     );
@@ -133,33 +127,31 @@ class DrawerMenuState extends State<DrawerMenu> {
       color: selected ? Colors.red[200] : Colors.transparent,
       child: InkWell(
         onTap: () {
-          setState(
-            () {
-              if (id == 1) {
-                currentPage = DrawerSections.library;
-              } else if (id == 2) {
-                currentPage = DrawerSections.stoury;
-              } else if (id == 3) {
-                currentPage = DrawerSections.podcast;
-              } else if (id == 4) {
-                currentPage = DrawerSections.quiz;
-              } else if (id == 5) {
-                currentPage = DrawerSections.marathon;
-              } else if (id == 6) {
-                currentPage = DrawerSections.proforintation;
-              } else if (id == 7) {
-                currentPage = DrawerSections.hobby;
-              } else if (id == 8) {
-                currentPage = DrawerSections.sport;
-              } else if (id == 9) {
-                currentPage = DrawerSections.cinema;
-              } else if (id == 10) {
-                currentPage = DrawerSections.new_recommendation;
-              }
-            },
-          );
           Navigator.of(context).pop();
           Navigator.push(context, materialPageRoute);
+          setState(() {
+            if (id == 1) {
+              currentPage = DrawerSections.library;
+            } else if (id == 2) {
+              currentPage = DrawerSections.stoury;
+            } else if (id == 3) {
+              currentPage = DrawerSections.podcast;
+            } else if (id == 4) {
+              currentPage = DrawerSections.quiz;
+            } else if (id == 5) {
+              currentPage = DrawerSections.marathon;
+            } else if (id == 6) {
+              currentPage = DrawerSections.proforintation;
+            } else if (id == 7) {
+              currentPage = DrawerSections.hobby;
+            } else if (id == 8) {
+              currentPage = DrawerSections.sport;
+            } else if (id == 9) {
+              currentPage = DrawerSections.cinema;
+            } else if (id == 10) {
+              currentPage = DrawerSections.new_recommendation;
+            }
+          });
         },
         child: Padding(
           padding: EdgeInsets.all(10.0),
