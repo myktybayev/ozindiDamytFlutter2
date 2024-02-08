a6c5fa2f18c75f1047ec2b14f1abcabc7cfad194
Erkebulan <tolendierkebulan555@gmail.com>
2024-02-08 11:09:28 +0600

    committed

--- lib/features/new_recommendation/new_recommendation_page.dart
+++ lib/features/new_recommendation/new_recommendation_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:ozindi_damyt/core/navigation/navigation.dart';
 
 class NewRecommendationPage extends StatelessWidget {
   const NewRecommendationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('New Recommendation page'),
-    );
+    return Scaffold(
+        drawer: DrawerMenu(),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60),
+          child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 3,
+                  blurRadius: 10,
+                )
+              ],
+            ),
+            child: AppBar(
+              title: Text('New Recommendation page'),
+            ),
+          ),
+        ),
+        body: Center(child: Text("New Recommendation page")));
   }
 }
